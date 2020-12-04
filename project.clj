913a0cc5abcaf33f399820114f6308f99e41765a
Oleh Kylymnyk <okilimnik@gmail.com>
2020-12-04 23:37:56 +0200

    fixes

--- project.clj
+++
@@ -1,9 +0,0 @@
-(defproject okilimnik/firebase "0.0.1"
-  :description "Firebase for Clojure(Script)."
-  :url "https://github.com/okilimnik/firebase"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.2-alpha1"]
-                 [binaryage/oops "0.7.0"]
-                 [org.clojure/core.async "1.1.587"]])
-
